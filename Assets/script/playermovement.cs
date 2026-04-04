39e337a65b0f3017db30ddc49279c251452772e4
Yu-Da-Shen <1625465428@qq.com>
2026-04-04 20:47:44 +0800

    导入插件制作简单的playerINput

--- Assets/script/playermovement.cs
+++ Assets/script/playermovement.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class playermovement : MonoBehaviour
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions; 
+public class playermovement : MonoBehaviour, PlayerInput.IGameModeActions
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("移动参数")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 720f;
+
+    private PlayerInput inputActions;
+    private Vector2 moveInput;
+    private Rigidbody rb;
+
+    private void Awake()
     {
-        
+        inputActions = new PlayerInput();
+        inputActions.GameMode.AddCallbacks(this);
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        inputActions.GameMode.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.GameMode.Disable();
+    }
+
+    private void OnDestroy()
+    {
+        inputActions.Dispose();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveInput.x, 0f, moveInput.y) * moveSpeed;
+        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
+
+        if (moveInput.sqrMagnitude > 0.01f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(new Vector3(moveInput.x, 0f, moveInput.y));
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+        }
     }
 }
